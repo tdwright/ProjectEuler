99e0f2dc828442bc599cf220ca3441df13bcc1c5
Tom Wright <Tom Wright>
2017-05-20 22:42:02 +0100

    Add project files.

---
+++ 001 - FivesAndThrees/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001___FivesAndThrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = Enumerable.Range(1, 1000).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+            Console.WriteLine($"{sum}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 002 - EvenFibb/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002___EvenFibb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int acc = b;
+
+            while (b<=4000000)
+            {
+                int oldA = a;
+                a = b;
+                b = oldA + b;
+                if (b%2==0) acc += b;
+                Console.WriteLine($"x = {b} ({acc})");
+            }
+            Console.WriteLine($"Final = {acc}");
+            Console.ReadLine();
+        }
+    }
+}
