472f3f8eda67ae96111c51972e755eaba463ca80
tdwright <tom@tdwright.co.uk>
2017-06-04 21:29:27 +0100

    Solved problems 10 and 11 (sum of primes and largest product in grid)

---
+++ 010 - SumOfPrimes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010___SumOfPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int limit = 2000000;
+            var primes = Sieve(limit);
+            Console.WriteLine(primes.Sum());
+            Console.ReadLine();
+        }
+
+        private static List<long> Sieve(int limit)
+        {
+            var numbers = Enumerable.Range(2, limit).ToList();
+
+            int cur = 1;
+
+            while (cur < numbers.Max())
+            {
+                cur = numbers.First(i => i > cur);
+                numbers.RemoveAll(i => i % cur == 0 && i != cur);
+                Console.WriteLine($"Removing multiples of {cur}. {numbers.Count} numbers remain.");
+            }
+
+            return numbers.Where(i => i < limit).Select(i => (long)i).ToList();
+        }
+    }
+}
